2672fbe684c60c09c60f481ccf9d1dbb5bf2ed75
nkalupahana <avishkank@gmail.com>
2020-01-31 22:20:19 +0000

    Code comments

--- connector.js
+++ connector.js
@@ -1,7 +1,10 @@
 /* global tableau reqwest */
 
 (function() {
+    // Dark Sky API Key
     var API_KEY = "c5a50d083981d1ecad8c5b22c76d2762";
+    
+    // Schema for data to get from Dark Sky JSON
     var cols = [{
             id: "time",
             dataType: tableau.dataTypeEnum.datetime
@@ -38,7 +41,7 @@
     // Create the connector object
     var connector = tableau.makeConnector();
 
-    // Define the schema
+    // Define schema
     connector.getSchema = function(schemaCallback) {
         var tableSchema = {
             id: "darkskyData",
@@ -49,23 +52,28 @@
         schemaCallback([tableSchema]);
     };
 
-    // Download the data
+    // Download and format data
     connector.getData = function(table, doneCallback) {
         reqwest({
-            url: "https://api.darksky.net/forecast/c5a50d083981d1ecad8c5b22c76d2762/45.535122,-122.948361",
+            url: `https://api.darksky.net/forecast/${API_KEY}/45.535122,-122.948361`,
             type: "jsonp",
             success: resp => {
+                // Combine hourly and daily data
                 let data = resp.hourly.data.concat(resp.daily.data);
                 let tableData = [];
                 
+                // Format data as necessary
                 for (let item of data) {
                     let obj = {};
                     
+                    // Cycle through data and pick out pieces in schema
                     for (let attributeToAdd of cols) {
                         if (attributeToAdd.id == "time") {
+                            // Create formatted DateTime object for Tableau
                             let tobj = new Date(item[attributeToAdd.id] * 1000);
                             obj[attributeToAdd.id] = tobj.toLocaleDateString() + " " + tobj.toLocaleTimeString();
                         } else if (attributeToAdd.id == "temperature") {
+                            // Average temperature if not included in Dark Sky
                             if (Object.keys(item).includes("temperature")) {
                                 obj[attributeToAdd.id] = item[attributeToAdd.id];
                             } else {
@@ -79,6 +87,7 @@
                     tableData.push(obj);
                 }
                 
+                // Send data to Tableau and mark as complete
                 table.appendRows(tableData);
                 doneCallback();
             }
@@ -87,6 +96,7 @@
 
     tableau.registerConnector(connector);
     
+    // Register event listener on ready
     if (document.readyState != 'loading'){
         ready();
     } else {
@@ -94,11 +104,12 @@
     }
 
 
-    // Create event listeners for when the user submits the form
+    // Create event listener for when user requests data
     function ready() {
         document.getElementById("submitButton").addEventListener("click", () => {
-            tableau.connectionName = "Dark Sky Connector"; // This will be the data source name in Tableau
-            tableau.submit(); // This sends the connector object to Tableau
+            // Set data source name and send to Tableau
+            tableau.connectionName = "Dark Sky Connector";
+            tableau.submit();
         });
     }
     
